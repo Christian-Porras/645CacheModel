368ab7c3faeaea512e57c5d6c6fe94630d908a69
Evan Bissell <ebissell@1005B-6.eecs.ku.edu>
2017-04-09 16:47:53 -0500

    Added place function

--- CacheSim.c
+++ CacheSim.c
@@ -11,3 +11,25 @@ int main(char[][] args){
 
   return 0;
 }
+
+boolean place(struct element){
+  for(int i = 0; i < associativity; i++){
+    if(cache[element.linenum][i].valid){
+      if(element.tag == cache[element.linenum][i].tag){
+        return 1;
+      }
+      else if(i == associativity-1){
+        bump(element);
+        break;
+      }
+    }
+    else{
+      cache[element.linenum][i] = element;
+      break;
+    }
+    else if (i == associativity-1){
+      bump(element);
+    }
+  }
+  return 0; 
+}
